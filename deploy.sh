f2e28351ee2b9aa4a8eb93804423915da7cbb41a
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-28 08:00:21 +0200

    Travis setup is Done for frontend Repo + initial deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#set -e
+
+#first clone pull the repo and pull the new images from docker hub ( sudo docker-compose pull &&sudo docker-compose down &&)
+#then docker-compose up -> reload the images..
+ssh -i ./deploy_key Dev@52.170.119.1 pwd #'cd Flicker_Clone_SWProject21_Team2/ && git clean  -d  -f . &&git reset --hard && git pull && sudo docker-compose pull && sudo docker-compose up -d'
