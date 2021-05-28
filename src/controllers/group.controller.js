7fc087b8dcaae2d7edfcd605d9005160b62c25ba
moustafaomar <moustafaomar200@gmail.com>
2021-05-29 01:19:17 +0200

    Added get group photos

--- src/controllers/group.controller.js
+++ src/controllers/group.controller.js
@@ -156,3 +156,50 @@ exports.leave = async function (req, res) {
     });
   }
 };
+
+exports.getGroupPhotos = async function (req, res) {
+  const userId = res.locals.userid;
+  const groupId = req.params.id;
+  let group;
+  try {
+    group = await Group.findById(groupId).lean().exec();
+  } catch (err) {
+    return res.status(500).json({ message: 'Invalid group ID' });
+  }
+  if (!groupId) {
+    res.status(422).json({ message: 'Missing group parameter' });
+  } else if (!group || (group && group.privacy === 'private')) {
+    res.status(404).json({ message: 'Group not found' });
+  } else {
+    const number = await Group.find({
+      _id: groupId,
+      Members: {
+        $elemMatch: { ref: userId },
+      },
+    }).exec();
+    if (number.length > 0) {
+      const memberRole = await Group.aggregate([
+        {
+          $match: { _id: { $eq: mongoose.Types.ObjectId(groupId) } },
+        },
+        {
+          $unwind: '$Members',
+        },
+        {
+          $match: {
+            'Members.ref': { $eq: mongoose.Types.ObjectId(userId) },
+          },
+        },
+        {
+          $project: {
+            ref: '$Members.ref',
+            role: '$Members.role',
+          },
+        },
+      ]).exec();
+      delete group.Members;
+      group.role = memberRole[0].role;
+    }
+    res.status(200).json(group);
+  }
+};
