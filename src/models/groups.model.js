d7baba2ae46fd3977fd041e46c2beb7ad403b364
moustafaomar <moustafaomar200@gmail.com>
2021-05-30 17:02:51 +0200

    Added createdAt

--- src/models/groups.model.js
+++ src/models/groups.model.js
@@ -1,54 +1,60 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const groupSchema = new mongoose.Schema(
-    {
-        Members: [
-            {
-                ref: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: 'User',
-                },
-                role: {
-                    type: String,
-                    default: 'member',
-                },
-                _id: false,
-            },
-        ],
-        Photos: {
-            type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Photo' }],
+  {
+    Members: [
+      {
+        ref: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
         },
-        name: {
-            type: String,
-            minlength: 1,
-            maxlength: 30,
-            required: true,
+        role: {
+          type: String,
+          default: 'member',
         },
-        description: {
-            type: String,
-            minlength: 1,
-            maxlength: 100,
-            default: null,
-        },
-        privacy: {
-            type: String,
-            enum: ['public', 'private'],
-            default: 'public',
-        },
-        visibility: {
-            type: String,
-            enum: ['public', 'invite'],
-            default: 'public',
-        },
+        _id: false,
+      },
+    ],
+    Photos: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Photo' }],
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        toObject: {
-            virtuals: true,
-        },
-    }
-)
-const Group = mongoose.model('Group', groupSchema)
+    name: {
+      type: String,
+      minlength: 1,
+      maxlength: 30,
+      required: true,
+    },
+    description: {
+      type: String,
+      minlength: 1,
+      maxlength: 100,
+      default: null,
+    },
+    privacy: {
+      type: String,
+      enum: ['public', 'private'],
+      default: 'public',
+    },
+    visibility: {
+      type: String,
+      enum: ['public', 'invite'],
+      default: 'public',
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      min: '2021-01-1',
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+const Group = mongoose.model('Group', groupSchema);
 
-module.exports = { Group, groupSchema }
+module.exports = { Group, groupSchema };
