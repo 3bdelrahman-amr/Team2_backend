d4ffd3d02ef2f5f8ac16c0e6c6126335b85dfc72
3bdelrahman-amr <52078214+3bdelrahman-amr@users.noreply.github.com>
2021-06-01 09:31:34 +0200

    cloudinary added in package.json

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.options('*', cors())
 
 // simple route for testing api is working:
 app.get("/api/test", (req, res) => {
-    res.json({ message: "Welcome to dropoids backend application V99" });
+    res.json({ message: "Welcome to dropoids backend application V100" });
   });
 
 
@@ -31,6 +31,6 @@ app.use('/api/v1',routes);
 // });
 
 
-app.use('/photos',express.static('photos')); // makes the photos folder available for everyone
+//app.use('/photos',express.static('photos')); // makes the photos folder available for everyone
 
 module.exports = app
